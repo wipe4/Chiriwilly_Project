e6f50b1abac283c6318b0e9587ce640c7d20bb40
jvlopez <jvlopez.clb@grupoprisa.com>
2021-05-29 22:22:59 +0200

    Test servos, movimiento cuqui

--- Robot_Mov/Robot_Mov.ino
+++ Robot_Mov/Robot_Mov.ino
@@ -1,23 +1,35 @@
 #include <Servo.h>
 
-Servo myservo;
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
 
 
 int pos = 90; //90º cabeza centrada
 
 /*=== Inicio del programa ===*/
 void setup() {
-  
-  myservo.attach(8);  //A Derecha   PIN 8
-  myservo.attach(9);  //B Izquierda PIN 9
-  myservo.attach(10); //C Arriba    PIN 10
+
+  myservo3.attach(8);  //A Derecha   PIN 8
+  myservo2.attach(9);  //B Izquierda PIN 9
+  myservo1.attach(10); //C Arriba    PIN 10
 
 }
 
 void loop() {
-  
+
   //Movimiento de cabeza
-  c_mirar (3000);
+  //c_mirar (3000);
+
+  
+  myservo2.write(pos + random(30, 180));
+  delay(300);
+  myservo3.write(pos - random(30, 180));
+  delay(300);
+  myservo1.write(pos - random(-80, 80));
+  sonido(200);
+  delay(500);
+  
 
 }
 
@@ -28,14 +40,15 @@ void loop() {
 /*C Arriba - Cabeza*/
 void c_mirar (int speed) {
 
-  myservo.write(pos);
+ 
+  myservo1.write(pos);
   sonido(100);
   delay(speed);
-  myservo.write(pos + random(1, 80));
+  myservo1.write(pos + random(1, 80));
   sonido(200);
   delay(speed);
-  myservo.write(pos - random(1, 80));
-  sonido(100);
+  myservo1.write(pos - random(1, 80));
+  sonido(50);
   delay(speed);
 
 
@@ -43,14 +56,14 @@ void c_mirar (int speed) {
 
 void c_no () {
 
-  myservo.write(pos + 45);
+  myservo1.write(pos + 45);
   delay(200);
-  myservo.write(pos - 45);
+  myservo1.write(pos - 45);
   delay(200);
-  myservo.write(pos + 45);
+  myservo1.write(pos + 45);
   delay(200);
-  myservo.write(pos - 45);
-  myservo.write(pos);
+  myservo1.write(pos - 45);
+  myservo1.write(pos);
   delay(1000);
 
 
@@ -65,7 +78,6 @@ void sonido (int speed) {
     delay(speed);
 
   }
-
 
 
 
